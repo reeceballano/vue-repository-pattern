d20ab972e65ca1fe49e30b1ca39792fdf152ecdb
Reece <trazhme@gmail.com>
2021-06-06 20:54:34 +0800

    fixed get paginate statement

--- src/repositories/PostRepository.js
+++ src/repositories/PostRepository.js
@@ -6,8 +6,8 @@ export default {
     get(paginate) {
         const { start, limit } = paginate;
         (Object.keys(paginate).length === 0) ? console.log('empty') :  console.log('not empty');
-        return (Object.keys(paginate)) ? Client.get(`${resource}?_start=${start}&_limit=${limit}`) : 
-                Client.get(`${resource}`);
+        return (Object.keys(paginate).length === 0) ? Client.get(`${resource}`) : 
+                Client.get(`${resource}?_start=${start}&_limit=${limit}`);
     },
 
     getPost(id) {
