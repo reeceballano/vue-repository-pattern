de1c5de9a2e444c1c553ebfc38ea268624de90c0
Reece <trazhme@gmail.com>
2021-06-17 07:31:34 +0800

    added: user view

---
+++ src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="user">
+        <h2>User Info:</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
