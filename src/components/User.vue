7c94819a0cc1057c18816ba3e25fa6fbe4f6fd91
Reece <trazhme@gmail.com>
2021-06-16 08:15:56 +0800

    added: user component

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user">
+        {{ user }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    props: {
+        user: {
+            type: Object,
+            default: () => {}
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
