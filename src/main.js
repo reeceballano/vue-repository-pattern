9ec35da8cb861f85a4f83148d7bbdcde97148892
Reece <trazhme@gmail.com>
2021-06-23 18:25:41 +0800

    fixed: tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './assets/css/styles.css'
+import '@/assets/css/styles.css'
 
 const app = createApp(App);
 
