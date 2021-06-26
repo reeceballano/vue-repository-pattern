8b9262340610f5d97ef6436db4de787c07cd5aec
Reece <trazhme@gmail.com>
2021-06-26 08:15:47 +0800

    added: home intro text

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/css/styles.css'
+import '@/assets/scss/styles.scss'
 import Default from './layouts/Default.vue'
 import NoSidebar from './layouts/NoSidebar.vue'
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="home">
+    <div class="home-content">
+        <section class="custom-container intro-text">
+            <h2>
+                Third fish itself winged midst, days. Living great dry moving, together gathering had i likeness over give man third, let.
+            </h2>
+
+            <div class="btn-center-wrapper">
+                <button class="btn-blue-outlined">
+                    Contact
+                </button>
+
+                <button class="btn-blue">
+                    Explore
+                </button>
+            </div>
+            
+        </section>
+
+
+
+
         {{ post }}
         <button @click="getPost">Get Post ID: 1</button>
 
@@ -60,3 +80,11 @@ export default {
     }
 }
 </script>
+
+<style scoped lang="scss">
+    .intro-text {
+        h2 {
+            @apply text-center text-center md:text-4xl text-2xl leading-10;
+        }
+    }
+</style>
