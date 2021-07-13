2ae73be94dbb47ecb236627491d20c238b248345
Reece <trazhme@gmail.com>
2021-07-13 22:27:49 +0800

    added: suspense on comment component

---
+++ src/components/Comment.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="comment-item">
+        {{ data }}
+        <strong>{{ data.comment.author }}</strong>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: 'Comment',
+    props: {
+        comment: {
+            type: Object,
+            default: () => {}
+        }
+    },
+
+    async setup(props) {
+        const data = ref(props);
+
+        return {
+            data
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CommentList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="comment-list">
+        <Comment v-for="comment in comments" :key="comment.id" :comment="comment" />
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Comment from '@/components/Comment';
+
+let fetchComments = new Promise((resolve) => {
+        const data = [
+            { id: 1, postId: 1, author: 'user 1', comment: 'comment 1 '},
+            { id: 2, postId: 2, author: 'user 2', comment: 'comment 2 '},
+        ]
+
+        setTimeout(() => {
+            return resolve(data)
+        }, 3000);
+})
+
+export default {
+    name: 'CommentList',
+    components: {
+        Comment,
+    },
+
+    async setup() {
+        const comments = ref(await fetchComments);
+
+        return {
+            comments,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
