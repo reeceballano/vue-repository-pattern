86ac4837c848314eeeb3184870b0d325efeaf25b
Reece <trazhme@gmail.com>
2021-08-28 17:58:05 +0800

    reused post component on search results items

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="search-results absolute bg-white w-full left-0 top-16 p-10 z-50">
         <input v-model="search" type="text" placeholder="Search" class="w-full border border-gray-100 px-5 py-3 focus:outline-none focus:ring-1"/>
-        {{ searchResults }}        
+
+        <Post v-for="post in searchResults" :key="post.id" :post="post" /> 
     </div>
 
 </template> 
@@ -9,9 +10,14 @@
 <script>
 import { ref, computed, onMounted, watch } from 'vue';
 import { useStore } from 'vuex';
+import Post from '@/components/Post';
 
 export default {
     name: 'Search',
+
+    components: {
+        Post,
+    },
 
     setup() {
         const store = useStore();
