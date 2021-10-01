5508d28faf4cbaf332dafa2ed6b8247c2d3e121d
Reece <trazhme@gmail.com>
2021-10-01 20:31:23 +0800

    added: sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="sidebar">
+        <h2 class="text-2xl">Sidebar</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+
+    setup() {
+        return {
+            
+        }    
+    },
+}
+</script>
