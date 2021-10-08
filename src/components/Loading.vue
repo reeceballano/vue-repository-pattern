ba57cb9bec8c1262ea9f7b70e933e6b24d595f1b
Reece <trazhme@gmail.com>
2021-10-08 20:02:30 +0800

    updated: loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loading">
+        <h3>Loading...</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
