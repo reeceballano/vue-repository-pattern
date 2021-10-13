d200fe01cb7618a434bbdd03e0db2e21dfab5550
Reece <trazhme@gmail.com>
2021-10-13 16:28:36 +0800

    added: loading store module

---
+++ src/store/modules/loading.js
@@ -0,0 +1,27 @@
+// STATE
+const state = {
+
+}
+
+// GETTERS
+const getters = {
+
+}
+
+// MUTATIONS
+const mutations = {
+
+}
+
+// ACTIONS
+const actions = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
