f0b14ab979abeeca6f07ceabe058a025577f2434
Reece <trazhme@gmail.com>
2021-10-13 17:53:00 +0800

    added: mutations

--- src/store/modules/loading.js
+++ src/store/modules/loading.js
@@ -1,6 +1,8 @@
 // STATE
 const state = {
-
+    size: 4,
+    animation: 'spin',
+    icon: 'Refresh'
 }
 
 // GETTERS
@@ -10,12 +12,32 @@ const getters = {
 
 // MUTATIONS
 const mutations = {
+    SET_SIZE(state, size) {
+        state.size = size;
+    },
 
+    SET_ANIMATION(state, animation) {
+        state.animation = animation;
+    },
+
+    SET_ICON(state, icon) {
+        state.icon = icon;
+    }
 }
 
 // ACTIONS
 const actions = {
+    setSize({ commit }, payload) {
+        commit('SET_SIZE', payload);
+    },
 
+    setAnimation({ commit }, payload) {
+        commit('SET_ANIMATION', payload);
+    },
+
+    setIcon({ commit }, payload) {
+        commit('SET_ICON', payload);
+    }
 }
 
 export default {
