83bdb31fba28ffd445539eedaa8aaa13c302c0a6
Reece <trazhme@gmail.com>
2021-10-29 19:23:34 +0800

    added sidebar placeholder items

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="sidebar  col-span-1 md:col-span-1">
         <section class="sidebar-title">
-            <h2 class="text-2xl">Sidebar</h2>
+            <div class="pb-5 mb-5 border-b">
+                <h2 class="text-2xl mb-5">Latest Posts</h2>
+                <ul>
+                    <li>Post 1</li>
+                    <li>Post 1</li>
+                    <li>Post 1</li>
+                    <li>Post 1</li>
+                    <li>Post 1</li>
+                </ul>
+            </div>
+
+            <div class="pb-5 mb-5 border-b">
+                <h2 class="text-2xl mb-5">Active Author</h2>
+                <ul>
+                    <li>Author 1</li>
+                    <li>Author 1</li>
+                    <li>Author 1</li>
+                    <li>Author 1</li>
+                    <li>Author 1</li>
+                </ul>
+            </div>
         </section>
     </div>
 </template>
