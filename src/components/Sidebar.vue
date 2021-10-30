c9a18e1779e0b2f53ad984cab46f8b9c720ef8f6
Reece <trazhme@gmail.com>
2021-10-30 18:52:57 +0800

    replaced static post items

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,11 +4,7 @@
             <div class="pb-5 mb-5 border-b">
                 <h2 class="text-2xl mb-5">Latest Posts</h2>
                 <ul>
-                    <li>Post 1</li>
-                    <li>Post 1</li>
-                    <li>Post 1</li>
-                    <li>Post 1</li>
-                    <li>Post 1</li>
+                    <li v-for="post in latestPosts" :key="post.id">{{ post.title }}</li>
                 </ul>
             </div>
 
