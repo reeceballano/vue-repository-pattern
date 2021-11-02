a3b13549897c2194170329820b6f7544da3750f0
Reece <trazhme@gmail.com>
2021-11-02 19:17:57 +0800

    added primary button

---
+++ src/components/Buttons/PrimaryButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link 
+        :to="{ name:'Post', params: { id: postData.id } }"
+        class="bg-indigo-100 text-indigo-700 text-base font-semibold px-6 py-2 rounded-lg hover:bg-indigo-400 delay-150 hover:transition-all">
+        <slot />
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'PrimaryButton',
+    props: {
+        button: {
+            type: Object,
+            default: () => {}
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
