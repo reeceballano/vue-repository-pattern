8623ab56c7037b20933719f92ac6d56c8b13cf9d
Reece <trazhme@gmail.com>
2021-11-04 19:34:04 +0800

    removed button icon default color

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -3,7 +3,7 @@
         :to="{ name:routeTo, params: buttonParams }"
         class="primary-button">
         <slot />
-        <Icon icon="MenuAlt2Icon" class="h-3 w-3 ml-2 text-blue-500"/>
+        <Icon icon="MenuAlt2Icon" class="h-3 w-3 ml-2"/>
     </router-link>
 </template>
 
@@ -30,7 +30,7 @@ export default {
 
 <style scoped>
     .primary-button {
-        @apply bg-indigo-100 text-indigo-700 text-base font-semibold px-6 py-2 rounded-lg  delay-150 inline-flex justify-center;
+        @apply bg-indigo-100 text-indigo-700 text-base font-semibold px-6 py-2 rounded-lg  delay-150 inline-flex items-center;
     }
 
     .primary-button:hover {
