df2b30a4234f6a141d844857b9278561e5139bfc
Reece <trazhme@gmail.com>
2021-11-04 19:23:45 +0800

    reused icon component

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -3,10 +3,13 @@
         :to="{ name:routeTo, params: buttonParams }"
         class="bg-indigo-100 text-indigo-700 text-base font-semibold px-6 py-2 rounded-lg hover:bg-indigo-400 delay-150 hover:transition-all">
         <slot />
+        <Icon icon="MenuAlt2Icon" class="h-5 w-5 mr-2 text-blue-500"/>
     </router-link>
 </template>
 
 <script>
+import Icon from '@/components/Icon';
+
 export default {
     name: 'PrimaryButton',
     props: {
@@ -18,6 +21,9 @@ export default {
             type: Object,
             default: () => {}
         }
+    },
+    components: {
+        Icon,
     }
 }
 </script>
