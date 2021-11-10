8b61bc4d533bf74621d5bdc3fc02187bc8606312
Reece <trazhme@gmail.com>
2021-11-10 18:05:21 +0800

    added divider component

---
+++ src/components/Divider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="divider"></div>
+</template>
+
+<script>
+
+export default {
+    name: 'Divider'
+}
+</script>
+
+<style scoped>
+    .divider {
+        @apply w-full h-0 my-10 border-b border-gray-400;
+    }
+</style>
