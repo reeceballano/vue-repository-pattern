58b8bffe6e9488a133d674b5b51ad0f10c7fce03
Reece <trazhme@gmail.com>
2021-11-15 19:24:33 +0800

    added close icon

--- src/components/Modals/Modal.vue
+++ src/components/Modals/Modal.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="modal-container">
         <div class="modal">
+            <div class="close-button absolute right-4 top-4">
+                <Icon icon="XIcon" width="5" />
+            </div>
+
             <h2 class="text-2xl">Modal Title</h2>
 
             <div class="modal-content">
@@ -16,8 +20,13 @@
 </template>
 
 <script>
+import Icon from '@/components/Icon';
+
 export default {
     name: 'Modal',
+    components: {
+        Icon
+    }
 }
 </script>
 
@@ -27,7 +36,7 @@ export default {
     }
 
     .modal {
-        @apply bg-white min-h-1/4 md:w-2/5 w-11/12 md:p-9 p-5 shadow-2xl rounded-xl;
+        @apply bg-white min-h-1/4 md:w-2/5 w-11/12 md:p-9 p-5 shadow-2xl rounded-xl relative;
     }
 
 </style>
