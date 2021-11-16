aff6989b6d255d9c64fc2cb3435ebfe08dd88221
Reece <trazhme@gmail.com>
2021-11-16 19:07:27 +0800

    added transition effect

--- src/components/Modals/Modal.vue
+++ src/components/Modals/Modal.vue
@@ -1,19 +1,38 @@
 <template>
     <div>
-        <div v-show="isVisible" class="modal-overlay"></div>
-        <div v-show="isVisible" class="modal-container">
-            <div class="modal">
-                <div @click="closeModal" class="close-button absolute right-4 top-4 cursor-pointer">
-                    <Icon icon="XIcon" :width="5" />
-                </div>
-                <h2 class="text-2xl"><slot name="modalTitle" /></h2>
+        <transition 
+                enter-active-class="transition-opacity delay-150" 
+                enter-from-class="opacity-0" 
+                enter-to-class="opacity-100" 
+                leave-active-class="transition-opacity duration-200" 
+                leave-from-class="opacity-100" 
+                leave-to-class="opacity-0"
+            >
+            <div v-show="isVisible" class="modal-overlay"></div>
+        </transition>
 
-                <div class="modal-content">
-                    <p><slot name="modalContent" /></p>
-                </div>
+        <transition 
+            enter-active-class="transition-opacity delay-100" 
+            enter-from-class="opacity-0" 
+            enter-to-class="opacity-100" 
+            leave-active-class="transition-opacity duration-150" 
+            leave-from-class="opacity-100" 
+            leave-to-class="opacity-0"
+        >
+            <div v-show="isVisible" class="modal-container">
+                    <div class="modal">
+                        <div @click="closeModal" class="close-button absolute right-4 top-4 cursor-pointer">
+                            <Icon icon="XIcon" :width="5" />
+                        </div>
+                        {{isVisible}}
+                        <h2 class="text-2xl"><slot name="modalTitle" /></h2>
+
+                        <div class="modal-content">
+                            <p><slot name="modalContent" /></p>
+                        </div>
+                    </div>
             </div>
-
-        </div>
+        </transition>
         <slot name="modalButton"/>
     </div>
 </template>
@@ -56,14 +75,18 @@ export default {
 <style scoped>
 
     .modal-overlay {
-        
+        @apply fixed w-screen h-screen bg-gray-800 opacity-80 top-0 left-0;
     }
     .modal-container {
-        @apply fixed w-screen h-screen top-0 left-0 bg-gray-800 opacity-100 flex items-center justify-center;
+        @apply fixed w-screen h-screen top-0 left-0 flex items-center justify-center;
     }
 
     .modal {
-        @apply bg-white min-h-1/4 md:w-2/5 w-11/12 md:p-9 p-5 shadow-2xl rounded-xl relative;
+        @apply bg-white min-h-1/4 md:w-2/5 w-11/12 md:p-9 p-5 shadow-2xl rounded-xl relative transform transition-colors;
+    }
+
+    .isActive {
+        @apply bg-red-600 delay-1000;
     }
 
 </style>
