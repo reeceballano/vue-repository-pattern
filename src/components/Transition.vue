78505d56d6ef083658ece9be06640e01267ef816
Reece <trazhme@gmail.com>
2021-11-18 17:24:30 +0800

    added a separate component for transition

---
+++ src/components/Transition.vue
@@ -0,0 +1,21 @@
+<template>
+    <transition 
+        enter-active-class="transition-opacity delay-150" 
+        enter-from-class="opacity-0" 
+        enter-to-class="opacity-100" 
+        leave-active-class="transition-opacity duration-200" 
+        leave-from-class="opacity-100" 
+        leave-to-class="opacity-0"
+    >
+        <slot />
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'Transition'
+}
+</script>
+
+<style>
+</style>
