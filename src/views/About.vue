b5e88b623d29b769ab03a25365055b5c04f70c24
Reece <trazhme@gmail.com>
2021-11-20 20:45:06 +0800

    created section for input component

--- src/views/About.vue
+++ src/views/About.vue
@@ -83,6 +83,14 @@
                     </template>
                 </Modal>
             </div>
+
+            <Divider bg="blue" />
+
+            <h2 class="text-xl mb-10">Input Components</h2>
+
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+            </div>
         </div>
         
     </section>
