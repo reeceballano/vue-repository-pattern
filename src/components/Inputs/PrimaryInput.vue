bbaee64035f7409703e33b6294c436a43976039f
Reece <trazhme@gmail.com>
2021-11-21 20:37:43 +0800

    update bottom margin

--- src/components/Inputs/PrimaryInput.vue
+++ src/components/Inputs/PrimaryInput.vue
@@ -37,7 +37,7 @@ export default {
 
 <style scoped>
     .primary-input {
-        @apply bg-gray-100 w-full px-4 py-3 ring-2 ring-gray-100 mb-5
+        @apply bg-gray-100 w-full px-4 py-3 ring-2 ring-gray-100 mb-1
                 hover:bg-white focus-within:bg-white focus:outline-none transition-colors;
     }
 </style>
