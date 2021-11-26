6d76db89ff2e1e6ddc2b2bc2ee7cf8b370b9be61
Reece <trazhme@gmail.com>
2021-11-26 19:10:14 +0800

    added inputData props as object for cleaner props

--- src/components/Inputs/Input.vue
+++ src/components/Inputs/Input.vue
@@ -1,10 +1,56 @@
 <template>
-  
+    <input 
+        :name="name"
+        :type="type" 
+        :value="modelValue" 
+        @input="updateValue($event.target.value)" 
+        class="primary-input" 
+        :placeholder="(label) ? '' : placeholder" 
+    />
 </template>
 
 <script>
 export default {
+    name: 'Input',
+    props: {
+        inputData: {
+            type: Object,
+            default: () => {}
+        },
+        
+        placeholder: {
+            type: String
+        },
 
+        name: {
+            type: String
+        },
+
+        type: {
+            type: String,
+            default: 'text'
+        },
+
+        modelValue: {
+            type: String,
+            default: ''
+        },
+
+        label: {
+            type: Boolean,
+            default: false
+        }
+    },
+    
+    setup(props, { emit }) {
+        const updateValue = (value) => {
+            emit('update:modelValue', value);
+        }
+
+        return {
+            updateValue
+        }
+    }
 }
 </script>
 
