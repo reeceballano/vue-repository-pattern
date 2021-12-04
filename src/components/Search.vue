44c0e9013d66bdfaa6c5face602a7fa2ea4f55d2
Reece <trazhme@gmail.com>
2021-12-04 20:51:36 +0800

    added scrollbar style

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -90,3 +90,22 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.search-results::-webkit-scrollbar-track {
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: #F5F5F5;
+}
+
+.search-results::-webkit-scrollbar {
+	width: 5px;
+	background-color: #F5F5F5;
+}
+
+.search-results::-webkit-scrollbar-thumb {
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: #555;
+}
+</style>
