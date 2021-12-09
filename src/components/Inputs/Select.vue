d4024fb38e3ad74b2d1889dac5d8631489707dd1
Reece <trazhme@gmail.com>
2021-12-09 18:31:47 +0800

    added option placeholder

--- src/components/Inputs/Select.vue
+++ src/components/Inputs/Select.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="select-container">
-        
+        <select>
+            <option value="">Option 1</option>
+            <option value="">Option 2</option>
+            <option value="">Option 3</option>
+            <option value="">Option 4</option>
+            <option value="">Option 5</option>
+        </select>
     </div>
 </template>
 
