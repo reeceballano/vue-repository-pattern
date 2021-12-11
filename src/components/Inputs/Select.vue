7548d494327d35969c169d7d21448d9f2c1dd89d
Reece <trazhme@gmail.com>
2021-12-11 17:34:53 +0800

    replaced placeholder option with options data

--- src/components/Inputs/Select.vue
+++ src/components/Inputs/Select.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="select-container">
         <select>
-            <option value="">Option 1</option>
-            <option value="">Option 2</option>
-            <option value="">Option 3</option>
-            <option value="">Option 4</option>
-            <option value="">Option 5</option>
+            <option v-for="(option,i) in options" :value="option">{{ option }}</option>
         </select>
     </div>
 </template>
