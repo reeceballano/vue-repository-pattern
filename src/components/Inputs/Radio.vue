8b89ff8418648b2aaa7a49c005ce0a23a54c42a8
Reece <trazhme@gmail.com>
2021-12-14 17:14:51 +0800

    updated option placeholder

--- src/components/Inputs/Radio.vue
+++ src/components/Inputs/Radio.vue
@@ -2,6 +2,10 @@
     <div class="radio-container">
         <label for="option1">Option 1</label>
         <input type="radio" name="option1" value="option 1" />
+        <label for="option2">Option 2</label>
+        <input type="radio" name="option1" value="option 1" />
+        <label for="option3">Option 3</label>
+        <input type="radio" name="option3" value="option 3" />
     </div>
 </template>
 
