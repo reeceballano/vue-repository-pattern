984aa5c6f43f1ce41ac4e19f849694b8ee540455
Reece <trazhme@gmail.com>
2021-12-14 17:13:43 +0800

    added placeholder option

--- src/components/Inputs/Radio.vue
+++ src/components/Inputs/Radio.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="radio-container"></div>
+    <div class="radio-container">
+        <label for="option1">Option 1</label>
+        <input type="radio" name="option1" value="option 1" />
+    </div>
 </template>
 
 <script>
