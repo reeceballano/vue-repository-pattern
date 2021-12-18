42fade7dee4c65bab060ee37f67177775b2dae66
Reece <trazhme@gmail.com>
2021-12-18 17:50:11 +0800

    replaced static type with prop

--- src/components/Inputs/Radio.vue
+++ src/components/Inputs/Radio.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="radio-container">
         <label for="option1">Option 1</label>
-        <input type="radio" name="option1" value="option 1" />
+        <input 
+            :type="inputData.type" 
+            name="option1" 
+            value="option 1" 
+        />
     </div>
 </template>
 
