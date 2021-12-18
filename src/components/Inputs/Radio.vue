2ed8082cea9d3855443a02666df7bca59b788c2c
Reece <trazhme@gmail.com>
2021-12-18 17:47:55 +0800

    removed radio item container

--- src/components/Inputs/Radio.vue
+++ src/components/Inputs/Radio.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="radio-container">
-        <div class="radio-item">
-            <label for="option1">Option 1</label>
-            <input type="radio" name="option1" value="option 1" />
-        </div>
+        <label for="option1">Option 1</label>
+        <input type="radio" name="option1" value="option 1" />
     </div>
 </template>
 
