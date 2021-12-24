2a18b7c06192ea5fda1a85ec381a998293b7de90
Reece <trazhme@gmail.com>
2021-12-24 16:56:38 +0800

    adjusted to make more readable

--- src/components/Inputs/RangeSlider.vue
+++ src/components/Inputs/RangeSlider.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="range-slider">
-        <label 
-        class="block mb-2">{{inputData.label}}</label>
-        <input :name="inputData.name" type="range" min="1" max="100" />
+        <label class="block mb-2">{{inputData.label}}</label>
+        <input 
+            :name="inputData.name" 
+            type="range" 
+            min="1" 
+            max="100" 
+        />
     </div>
 </template>
 
