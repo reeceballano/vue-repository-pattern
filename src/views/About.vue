7b256f8e5c49e313d8e68bc71616c1c7470b305a
Reece <trazhme@gmail.com>
2021-12-29 17:53:58 +0800

    added option value for min and max

--- src/views/About.vue
+++ src/views/About.vue
@@ -189,7 +189,7 @@ export default {
             { id: 'field-checkbox', type: 'checkbox', label: 'Position', value:[], option:['Developer', 'Designer', 'DevOp'] },
             { id: 'field-select', type: 'select', label: 'Salary Expectation', value:'', option:['50000-70000', '70000-90000', '90000-120000'] },
             { id: 'field-radio', type: 'radio', label: 'Experience', value:'', option:['Junior', 'Mid', 'Senior'] },
-            { id: 'field-range-slider', type: 'range', label:'Annual Income', value: '' }
+            { id: 'field-range-slider', type: 'range', label:'Annual Income', value: [], option:[1000,5000] }
         ])
 
         return {
