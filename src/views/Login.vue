d9362a71d72ee14e0361e4e38996c37e51f7e317
Reece <trazhme@gmail.com>
2022-01-07 19:01:33 +0800

    updated login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,10 @@
     <div class="login">
         <section class="section">
             <div class="custom-container">
-                <h2 class="text-2xl mb-5">Login</h2>
+                <div class="md:w-1/2 mx-auto h-full">
+                    <h2 class="text-2xl mb-5">Login</h2>
+
+                </div>
             </div>
         </section>
     </div>
@@ -10,7 +13,7 @@
 
 <script>
 export default {
-
+    name: 'LoginView'
 }
 </script>
 
