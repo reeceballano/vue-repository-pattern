867bf4ec24b1fb099bb2c4764e6c10e6f88284a2
Reece <trazhme@gmail.com>
2022-01-09 20:07:54 +0800

    added margin

--- src/components/Header/Nav.vue
+++ src/components/Header/Nav.vue
@@ -35,7 +35,7 @@
                         <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
                     </div>
                     
-                    <div class="hidden sm:block sm:ml-6">
+                    <div class="hidden sm:block sm:ml-6 sm:mt-1.5">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                             <slot name="desktop"></slot>
