833b1db96fedd2a14447d23d879f25ab5c19df87
Reece <trazhme@gmail.com>
2022-01-09 20:12:48 +0800

    adjusted border radius

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -85,7 +85,7 @@ export default {
 
 <style scoped>
     .primary-button {
-        @apply text-center text-gray-700 text-base font-semibold px-6 py-4 rounded-lg  delay-150  inline-flex items-center justify-center;
+        @apply text-center text-gray-700 text-base font-semibold px-6 py-4 rounded-md  delay-150  inline-flex items-center justify-center;
     }
 
     .primary-button:hover {
