136fc7faf758ee3cae775e031e2d60ab290d258c
Reece <trazhme@gmail.com>
2022-01-10 18:29:48 +0800

    added register button

--- src/components/Header/Nav.vue
+++ src/components/Header/Nav.vue
@@ -43,8 +43,12 @@
                     </div>
 
                     <div class="user-utility md:w-full md:text-right md:block hidden sm:mt-1.5">
-                        <PrimaryButton css-style="h-10 uppercase" bg="blue" type="link" routeTo="/login">
+                        <PrimaryButton css-style="h-10 uppercase text-sm" bg="yellow" type="link" routeTo="/login">
                             Login
+                        </PrimaryButton>
+
+                        <PrimaryButton css-style="h-10 uppercase ml-2 text-sm" bg="red" type="link" routeTo="/login">
+                            Register
                         </PrimaryButton>
                     </div>
                 </div>
