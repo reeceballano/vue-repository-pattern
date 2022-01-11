72c02718fc7e2a6a025c132b01ea89255ccabfe3
Reece <trazhme@gmail.com>
2022-01-11 18:15:32 +0800

    updated bg color

--- src/components/Header/Nav.vue
+++ src/components/Header/Nav.vue
@@ -43,7 +43,7 @@
                     </div>
 
                     <div class="user-utility md:w-full md:text-right md:block hidden sm:mt-1.5">
-                        <PrimaryButton css-style="h-10 uppercase text-sm" bg="yellow" type="link" routeTo="/login">
+                        <PrimaryButton css-style="h-10 uppercase text-sm" bg="gray" type="link" routeTo="/login">
                             Login
                         </PrimaryButton>
 
