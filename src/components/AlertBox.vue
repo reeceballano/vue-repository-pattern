dc178eae46fa08e6793d6b88d03bf55169173340
Reece <trazhme@gmail.com>
2022-01-21 21:02:13 +0800

    created a computed property for alert type

--- src/components/AlertBox.vue
+++ src/components/AlertBox.vue
@@ -7,12 +7,24 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
     name: 'AlertBox',
     props: {
         alertType: {
             type: String,
             default: 'success'
+        }
+    },
+
+    setup(props) {
+        const alert = computed(() => {
+            return props.alertType;
+        })
+
+        return {
+            alert,
         }
     }
 }
