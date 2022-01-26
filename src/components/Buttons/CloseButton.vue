4ea9ba9d32a3b03a6a20f4a415559402f6058fdc
Reece <trazhme@gmail.com>
2022-01-26 10:34:42 +0800

    added container

--- src/components/Buttons/CloseButton.vue
+++ src/components/Buttons/CloseButton.vue
@@ -1,5 +1,7 @@
 <template>
-  
+    <div class="hover:bg-red-200 hover:text-red-800 rounded transition-colors close-button absolute right-4 top-4 cursor-pointer">
+
+    </div>
 </template>
 
 <script>
