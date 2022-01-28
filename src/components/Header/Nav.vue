cfa95eeadc49a1546b97f248820ed994e214ac6c
Reece <trazhme@gmail.com>
2022-01-28 16:41:32 +0800

    removed border radius

--- src/components/Header/Nav.vue
+++ src/components/Header/Nav.vue
@@ -43,11 +43,11 @@
                     </div>
 
                     <div class="user-utility md:w-full md:text-right md:block hidden sm:mt-1.5">
-                        <PrimaryButton css-style="h-10 uppercase text-sm text-white bg-indigo-500 hover:bg-blue-200 rounded" type="link" routeTo="/login">
+                        <PrimaryButton css-style="h-10 uppercase text-sm text-white bg-indigo-500 hover:bg-blue-200" type="link" routeTo="/login">
                             Login
                         </PrimaryButton>
 
-                        <PrimaryButton css-style="h-10 uppercase ml-2 text-sm text-white bg-red-500 hover:bg-red-200 rounded" type="link" routeTo="/login">
+                        <PrimaryButton css-style="h-10 uppercase ml-2 text-sm text-white bg-red-500 hover:bg-red-200" type="link" routeTo="/login">
                             Register
                         </PrimaryButton>
                     </div>
