930d251b6598195e0afc6705415523094fcaeb9c
Reece <trazhme@gmail.com>
2022-02-05 17:37:46 +0800

    removed transition inside alertbox component

--- src/components/AlertBox.vue
+++ src/components/AlertBox.vue
@@ -1,22 +1,16 @@
 <template>
-    <AnimateTransition>
-        <div class="box-alert">
-            <div :class="`bg-${alert}-400`" class="p-4 w-full mb-5">
-                <slot />
-            </div>
+    <div class="box-alert">
+        <div :class="`bg-${alert}-400`" class="p-4 w-full mb-5">
+            <slot />
         </div>
-    </AnimateTransition>    
+    </div>
 </template>
 
 <script>
 import { computed } from 'vue';
-import AnimateTransition from '@/components/AnimateTransition';
 
 export default {
     name: 'AlertBox',
-    components: {
-        AnimateTransition
-    },
     props: {
         alertType: {
             type: String,
