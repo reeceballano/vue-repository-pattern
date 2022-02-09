e85ed6ff505ff7e7cd2f805092eef251adce69d4
Reece <trazhme@gmail.com>
2022-02-09 18:16:01 +0800

    updated layout

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="register">
+        <section class="section">
+            <div class="custom-container">
+                <div class="md:w-1/2 mx-auto p-10 rounded shadow-2xl bg-white">
+                    <h2 class="text-2xl mb-5">Register</h2>
 
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Register',
-    
+
 }
 </script>
 
