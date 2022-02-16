30c18ddcf274dd27b761ca65d4421a448d063eaa
Reece <trazhme@gmail.com>
2022-02-16 18:13:01 +0800

    added alert type

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,7 +4,8 @@
             <div class="custom-container">
                 <div class="md:w-1/2 mx-auto p-10 rounded shadow-2xl bg-white">
                     <h2 class="text-2xl mb-5">Register</h2>
-                    <AlertBox v-if="isRegistered">
+                    {{isRegistered}}
+                    <AlertBox v-if="isRegistered != null" :alert-type="(isRegistered) ? 'success': 'error'">
                         Success! Registered Successfully!
                     </AlertBox>
                     <form>
