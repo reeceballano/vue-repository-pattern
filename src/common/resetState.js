f3c70f7fbb37824bf647fe54bb7b1d9f526e00b3
Reece <trazhme@gmail.com>
2022-02-24 18:00:15 +0800

    code cleanup

--- src/common/resetState.js
+++ src/common/resetState.js
@@ -7,9 +7,7 @@ const resetState = (state, cb) => {
     const newState = state;
     setTimeout(() => {
         newState.value = null;
-        console.log('reset')
         if(typeof cb === 'function') {
-            console.log('cb')
             cb();
         }
     }, 3000)
