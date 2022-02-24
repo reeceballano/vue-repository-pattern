4fa373b84b9d43435e5ddc9a957e49e993b4c632
Reece <trazhme@gmail.com>
2022-02-24 18:11:26 +0800

    added dummy data

--- src/components/Accordion/Accordion.vue
+++ src/components/Accordion/Accordion.vue
@@ -1,5 +1,17 @@
 <template>
-  
+    <div class="accordion">
+        <ul class="flex justify-start md:flex-row flex-col">
+            <li class="accordion-title">
+                Accordion 1
+            </li>
+            <li class="accordion-title">
+                Accordion 2
+            </li>
+            <li class="accordion-title">
+                Accordion 3
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -8,6 +20,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    .accordion-title {
+        @apply px-10 py-3 bg-gray-100 cursor-pointer flex justify-around items-center;
+    }
 
+    .active {
+        @apply bg-white border-b;
+    }
 </style>
