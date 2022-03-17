9517ba7a8427e8b84b5f5b1ae4aca4e7703a1452
Reece <trazhme@gmail.com>
2022-03-17 19:28:02 +0800

    created title component

---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2 class="hover:opacity-20 delay-50 transition-opacity text-xl font-semibold text-white sm:text-2xl sm:leading-7 sm:text-black md:text-3xl">
+        <slot />
+    </h2>    
+</template>
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+
+<style>
+</style>
