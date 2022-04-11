810e9f1f74bf93148e18dd8925223c92e5f1b659
Reece <trazhme@gmail.com>
2022-04-11 15:23:02 +0800

    used vuex store for button bg

--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
 export default {
     name: 'SecondaryButton',
     props: {
@@ -16,6 +19,29 @@ export default {
             type: String,
             default: 'text-gray-700 text-base font-semibold rounded'
         },
+    },
+
+    setup(props) {
+        const store = useStore();
+
+        const buttonSettings = computed(() => {
+            return store.getters['siteSetting/getButtonSetting'];
+        })
+
+        const buttonBg = computed(() => {
+            const checkBg = props.cssStyle;
+            
+            if(checkBg.includes('bg-')) {
+                return props.cssStyle;
+            } else {
+                console.log('props store', buttonSettings.value.background)
+                return buttonSettings.value.background;
+            }
+        })
+
+        return {
+            buttonBg
+        }
     }
 }
 </script>
