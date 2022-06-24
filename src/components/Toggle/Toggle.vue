1581b8ceb616214ad128068114fb436df94a0794
Reece <trazhme@gmail.com>
2022-06-24 19:53:51 +0800

    added checbox elements

--- src/components/Toggle/Toggle.vue
+++ src/components/Toggle/Toggle.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="toggle">
-
+    <div class="toggle flex items-center justify-center w-full mb-12">
+        <label class="flex items-center cursor-pointer">
+            <div class="relative">
+                <input type="checkbox" />
+                <!-- line -->
+                <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                <!-- dot -->
+                <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+            </div>
+            <!-- label -->
+            <div class="ml-3 text-gray-700 font-medium">
+            Toggle Me!
+            </div>
+        </label>
     </div>
 </template>
 
