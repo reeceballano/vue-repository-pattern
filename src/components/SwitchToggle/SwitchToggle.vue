fdfc0bda0320f62b5066eb7ee3028945aa43c7d3
Reece <trazhme@gmail.com>
2022-07-07 19:32:23 +0800

    added class binding

--- src/components/SwitchToggle/SwitchToggle.vue
+++ src/components/SwitchToggle/SwitchToggle.vue
@@ -16,7 +16,7 @@
                     <!-- line -->
                     <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
                     <!-- dot -->
-                    <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                    <div :class="checkValue(option) ? 'inputCheck' : ''" class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
                 </div>
                 <!-- label -->
                 <div class="ml-3 text-gray-700 font-medium">
@@ -79,6 +79,7 @@
 </script>
 
 <style scoped>
+    .inputCheck,
     input:checked ~ .dot {
         transform: translateX(100%);
         background-color: #12c0f0;
