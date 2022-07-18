783b5e445d7aeff43fdca32b6f8969af12e7b592
Reece <trazhme@gmail.com>
2022-07-18 16:04:28 +0800

    updated todo

--- src/components/Inputs/Checkbox.vue
+++ src/components/Inputs/Checkbox.vue
@@ -21,7 +21,7 @@
     /**
     * TODO
     * - DONE. check if checkbox has value on initial page load
-    * - fix the value of collectedValues, check if there is a value in inputData.value 
+    * - DONE. fix the value of collectedValues, check if there is a value in inputData.value 
     */
 import { ref } from 'vue';
 import { checkValue as checkVal } from '@/common/checkValue';
