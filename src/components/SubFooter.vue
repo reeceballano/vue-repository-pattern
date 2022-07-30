4cee86f3ae1938f4c73b4ef94cd84bd58ce560a0
Reece <trazhme@gmail.com>
2022-07-30 18:20:20 +0800

    updated

--- src/components/SubFooter.vue
+++ src/components/SubFooter.vue
@@ -2,15 +2,40 @@
     <section class="sub-footer">
         <div class="custom-container">
             <div class="grid grid-cols-4 gap-4">
-                <h2>Item Title</h2>
+                <SubFooterItem 
+                    v-for="item in items"
+                    :key="item.id"
+                    :item="item"
+                >
+                    <template #Item>
+                        {{item.content}}
+                    </template> 
+                </SubFooterItem>
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import { ref } from 'vue';
+import SubFooterItem from '@/components/SubFooterItem';
+
 export default {
     name: 'SubFooter',
+    components: {
+        SubFooterItem
+    },
+    setup() {
+        const items = ref([
+            { id: 1, name: 'Item1', title: 'Item 1', content: 'Item 1 content' },
+            { id: 2, name: 'Item2', title: 'Item 2', content: 'Item 2 content' },
+            { id: 3, name: 'Item3', title: 'Item 3', content: 'Item 3 content' },
+        ])
+
+        return {
+            items
+        }
+    }
 }
 </script>
 
