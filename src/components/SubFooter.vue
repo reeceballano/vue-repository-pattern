f8e7ee896cdb0c9084f894d542bd208dce493d15
Reece <trazhme@gmail.com>
2022-07-30 17:28:40 +0800

    updated

--- src/components/SubFooter.vue
+++ src/components/SubFooter.vue
@@ -2,7 +2,7 @@
     <section class="sub-footer">
         <div class="custom-container">
             <div class="grid grid-cols-4 gap-4">
-
+                <h2>Item Title</h2>
             </div>
         </div>
     </section>
@@ -10,9 +10,12 @@
 
 <script>
 export default {
-
+    name: 'SubFooter',
 }
 </script>
 
 <style scoped>
+    h2 {
+        @apply text-xl font-medium;
+    }
 </style>
