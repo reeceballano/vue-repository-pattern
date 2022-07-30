72b53744108c401c8e50ecdc7b9168001cb092e9
Reece <trazhme@gmail.com>
2022-07-30 18:19:56 +0800

    created SubFooterItem component

---
+++ src/components/SubFooterItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="subfooter-item">
+        <h2>{{item.title}}</h2>
+        <slot name="Item" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SubFooterItem',
+    props: {
+        item: {
+            type: Object,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
