9d4ffd06cc5715b74fafba9f311d241ef575cbf1
Reece <trazhme@gmail.com>
2022-08-01 17:00:51 +0800

    removed name properties

--- src/components/SubFooter.vue
+++ src/components/SubFooter.vue
@@ -28,10 +28,10 @@ export default {
     },
     setup() {
         const items = ref([
-            { id: 1, name: 'Item1', title: 'Item 1', content: 'Item 1 content' },
-            { id: 2, name: 'Item2', title: 'Item 2', content: 'Item 2 content' },
-            { id: 3, name: 'Item3', title: 'Item 3', content: '<p>Item 3 <strong>content</strong></p>' },
-            { id: 4, name: 'Item4', title: 'Item 4', content: '<p>Item 4' },
+            { id: 1, title: 'Item 1', content: 'Item 1 content' },
+            { id: 2, title: 'Item 2', content: 'Item 2 content' },
+            { id: 3, title: 'Item 3', content: '<p>Item 3 <strong>content</strong></p>' },
+            { id: 4, title: 'Item 4', content: '<p>Item 4' },
         ])
 
         return {
