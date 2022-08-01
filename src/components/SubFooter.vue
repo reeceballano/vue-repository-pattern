d09b6dd81c6f8ac46617ec32e4253edb3f7201de
Reece <trazhme@gmail.com>
2022-08-01 16:58:19 +0800

    added footer item object

--- src/components/SubFooter.vue
+++ src/components/SubFooter.vue
@@ -31,6 +31,7 @@ export default {
             { id: 1, name: 'Item1', title: 'Item 1', content: 'Item 1 content' },
             { id: 2, name: 'Item2', title: 'Item 2', content: 'Item 2 content' },
             { id: 3, name: 'Item3', title: 'Item 3', content: '<p>Item 3 <strong>content</strong></p>' },
+            { id: 4, name: 'Item4', title: 'Item 4', content: '<p>Item 4' },
         ])
 
         return {
